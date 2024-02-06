1d7a7ca30e19f0fd44afb9d9c994a8aac0ce1364
Kristoforus Gwayne Donovan Muliadi <98434053+micropeltes@users.noreply.github.com>
2024-02-06 17:18:57 +0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -25,19 +25,28 @@ basic.showLeds(`
     `)
 basic.pause(1000)
 basic.showLeds(`
+    . . # # #
+    . . # . #
+    . . # . #
+    . . # . #
+    . . # # #
+    `)
+basic.pause(1000)
+basic.showLeds(`
     . . . . .
     . . . . .
     . . . . .
     . . . . .
     . . . . .
     `)
+motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, 125)
+basic.pause(70)
 basic.forever(function () {
-    while (true) {
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 100)
-        basic.pause(100)
-        maqueen.motorStop(maqueen.Motors.M1)
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 100)
-        basic.pause(100)
-        maqueen.motorStop(maqueen.Motors.M1)
-    }
+    motor.MotorRun(motor.Motors.M1, motor.Dir.CW, 125)
+    basic.pause(140)
+    motor.motorStop(motor.Motors.M1)
+    basic.pause(100)
+    motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, 125)
+    basic.pause(140)
+    motor.motorStop(motor.Motors.M1)
 })
